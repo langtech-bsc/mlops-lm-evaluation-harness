5dd388b2fe8a9938eea8a5169b3c7b0b19078f5c
Javi GG <s.javi.com@gmail.com>
2024-11-26 12:04:09 +0100

    Update 04_Gender_Bias.py

--- app/pages/04_Gender_Bias.py
+++ app/pages/04_Gender_Bias.py
@@ -149,7 +149,58 @@ def main():
                 st.warning("Corpus-Level Metrics require data from at least one model. Please select a model to proceed.")
 
         with tab2:
-            pass
+            
+
+            set_selected, target_col, models_selected = st.columns(3)
+
+            available_sets = [i for i in df['dataset'].unique() if 'mmhb' in i]
+
+            with set_selected:
+                selected_set = st.selectbox('Select Subset', available_sets)
+
+            with target_col:
+                target_selected = st.selectbox('Select Target Language', sorted( df_must_she['target'].unique() ) )
+
+            df_must_she_filtered = df_must_she.loc[df_must_she['target'] == target_selected]
+
+            with models_selected:
+                selected_models = st.multiselect('Select models to compare', options = df_must_she_filtered['model_name'].unique())
+
+            st.dataframe(df_must_she_filtered)
+            JSONS = []
+            NAMES = []
+            if not df_must_she_filtered.empty:    
+                for index, row in df_must_she_filtered.iterrows():
+                    # Get the model_name and file_name
+                    model_name = row['model_name']
+                    file_name = row['file_name']
+                    
+                    file_path = os.path.join(DATA_PATH, model_name, file_name)
+                    JSONS.append(file_path)
+                    NAMES.append(model_name)
+            else:
+                st.write('No data available for the selected combination.')
+
+
+            # Load the JSON data
+            def load_data(json_path):
+                with open(json_path, 'r', encoding='utf8') as f:
+                    data = json.load(f)
+                first_key = next(iter(data['results']))
+                return data['results'][first_key]
+
+
+            # load models data
+            DATA_MODELS = {}
+
+            for modelname, json_path in zip(NAMES, JSONS):
+                if modelname in selected_models:
+                    data = load_data(json_path)
+                    # Extract corpus-level metrics, if they are not computed, 
+                    # then assign the corresponding default value, -1000 for 
+                    # high is better, +1000 for lower is better metrics
+
+
     else:
         st.error(f"File not found: {DATA_PATH_csv}. Please ensure the file exists.")
 
